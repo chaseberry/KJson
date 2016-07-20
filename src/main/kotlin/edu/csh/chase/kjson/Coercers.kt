c708621187d1e62014a06ecd1ef5d12ee0521c52
Chase <chase@csh.rit.edu>
2016-07-20 10:01:51 -0400

    Added a Coercer for Booleans

--- src/main/kotlin/edu/csh/chase/kjson/Coercers.kt
+++ src/main/kotlin/edu/csh/chase/kjson/Coercers.kt
@@ -2,11 +2,18 @@ package edu.csh.chase.kjson
 
 object Coercers {
 
+    fun toBoolean(value: Any?): Boolean? {
+        return when (value) {
+            is Boolean? -> value
+            else -> java.lang.Boolean.parseBoolean(value.toString())
+        }
+    }
+
     fun toInt(value: Any?): Int? {
 
         return when (value) {
-            is Int -> value
-            is Double -> value.toInt()
+            is Int? -> value
+            is Number? -> value?.toInt()
             else -> {
                 try {
                     value.toString().toInt()
@@ -19,8 +26,8 @@ object Coercers {
 
     fun toDouble(value: Any?): Double? {
         return when (value) {
-            is Double -> value
-            is Int -> value.toDouble()
+            is Double? -> value
+            is Number? -> value?.toDouble()
             else -> {
                 try {
                     value.toString().toDouble()
