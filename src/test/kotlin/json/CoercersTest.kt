7aa2e670eb4478b2b26c947622779066bf79869a
Chase <chase@csh.rit.edu>
2016-07-21 10:41:24 -0400

    Updated the Coercers test

--- src/test/kotlin/json/CoercersTest.kt
+++ src/test/kotlin/json/CoercersTest.kt
@@ -11,17 +11,17 @@ class CoercersTest {
                 "string" to "test",
                 "double" to 25.5)
 
-        assertEquals(5, json.getInt("int", true))
-        assertEquals(5, json.getInt("int", 4, true))
+        assertEquals(5, json.coerceInt("int"))
+        assertEquals(5, json.coerceInt("int", 4))
 
-        assertEquals(null, json.getInt("null", true))
-        assertEquals(1, json.getInt("null", 1, true))
+        assertEquals(null, json.coerceInt("null"))
+        assertEquals(1, json.coerceInt("null", 1))
 
-        assertEquals(null, json.getInt("string", true))
-        assertEquals(1, json.getInt("string", 1, true))
+        assertEquals(null, json.coerceInt("string"))
+        assertEquals(1, json.coerceInt("string", 1))
 
-        assertEquals(25, json.getInt("double", true))
-        assertEquals(25, json.getInt("double", 4, true))
+        assertEquals(25, json.coerceInt("double"))
+        assertEquals(25, json.coerceInt("double", 4))
     }
 
 
